583b37631c2337dfe63158122f0561c0d5df44ac
tomaszdworniczak <tomasz.dworniczak@start-up.house>
2021-08-16 13:05:42 +0200

    Add routing in main app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import {productsRouters} from "./routers/products.routes";
+import {usersRouters} from "./routers/users.routes";
 
 const app = express();
-const port = 3000;
+const port = 3030;
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/products', productsRouters());
+app.use('/api/users', usersRouters());
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
